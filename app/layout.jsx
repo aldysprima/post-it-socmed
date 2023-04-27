3e7a8b27b39462c458c2349698af2017f8d89203
Aldy Prima <Aldy.Prima@ipg.com>
2023-04-28 00:14:02 +0700

    adding layout file

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import Nav from "./auth/Nav";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <Nav />
+        {children}
+      </body>
+    </html>
+  );
+}
